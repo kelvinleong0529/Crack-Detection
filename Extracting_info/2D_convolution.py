b6936caee53dce7b4dc92aa3a9c70ba49506d363
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2023-05-31 15:55:53 +0800

    Update: refactor extracting-info

--- Extracting_info/2D_convolution.py
+++ Extracting_info/2D_convolution.py
@@ -2,12 +2,29 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-img = cv.imread('Extracting_info/cracks.jpg')
-kernel = np.ones((5,5),np.float32)/25
-dst = cv.filter2D(img,-1,kernel)
-plt.subplot(121),plt.imshow(img),plt.title('Original')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
-plt.xticks([]), plt.yticks([])
-cv.imwrite('Extracting_info/bilateral_result.jpg', dst)
-plt.show()
+def apply_filter(image_path, kernel):
+    img = cv.imread(image_path)
+    dst = cv.filter2D(img, -1, kernel)
+    return dst
+
+def show_images(original, filtered):
+    plt.subplot(121), plt.imshow(original), plt.title('Original')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(filtered), plt.title('Filtered')
+    plt.xticks([]), plt.yticks([])
+    plt.show()
+
+def save_image(image, output_path):
+    cv.imwrite(output_path, image)
+
+def main():
+    img_path = 'Extracting_info/cracks.jpg'
+    output_path = 'Extracting_info/bilateral_result.jpg'
+    kernel = np.ones((5, 5), np.float32) / 25
+
+    filtered_img = apply_filter(img_path, kernel)
+    show_images(cv.imread(img_path), filtered_img)
+    save_image(filtered_img, output_path)
+
+if __name__ == '__main__':
+    main()
