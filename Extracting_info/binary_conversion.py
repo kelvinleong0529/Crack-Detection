59946163355afce0518e2271c7ffc00535e3bdf8
Leong Zhin Sheng <zhinsheng97@hotmail.com>
2023-05-31 15:56:44 +0800

    Update: refactor binary_conversion

--- Extracting_info/binary_conversion.py
+++ Extracting_info/binary_conversion.py
@@ -1,6 +1,19 @@
 import cv2
-img = cv2.imread('Extracting_info/negative_image.png',2)
-ret, bw_img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-cv2.imshow("Binary Image",bw_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+def process_image(image_path):
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    ret, bw_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    return bw_img
+
+def show_image(image):
+    cv2.imshow("Binary Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def main():
+    image_path = 'Extracting_info/negative_image.png'
+    binary_image = process_image(image_path)
+    show_image(binary_image)
+
+if __name__ == '__main__':
+    main()
